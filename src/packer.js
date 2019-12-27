d6beb85fa5a0ff7d424ecb5e76107a4899bae2f7
rei2hu <r2hkri@gmail.com>
2019-12-27 11:23:51 -0600

    take buffers too

--- src/packer.js
+++ src/packer.js
@@ -1,18 +1,12 @@
-const { readFile, writeFileSync } = require("fs");
-const { promisify } = require("util");
 const { BMP_HEAD, BMP_END } = require("./constants");
-const promReadFile = promisify(readFile);
 
-module.exports = (imageFile, scriptFile) =>
-  Promise.all([promReadFile(imageFile), promReadFile(scriptFile)]).then(
-    ([imageBuf, scriptBuf]) => {
-      const bufs = [
-        Buffer.from(BMP_HEAD),
-        imageBuf.slice(BMP_HEAD.length),
-        Buffer.from(BMP_END),
-        scriptBuf
-      ];
-      const length = bufs.reduce((a, b) => a + b.length, 0);
-      return Buffer.concat(bufs, length);
-    }
-  );
+module.exports = (imageBuf, scriptBuf) => {
+  const bufs = [
+    Buffer.from(BMP_HEAD),
+    imageBuf.slice(BMP_HEAD.length),
+    Buffer.from(BMP_END),
+    scriptBuf
+  ];
+  const length = bufs.reduce((a, b) => a + b.length, 0);
+  return Buffer.concat(bufs, length);
+};
--- test/index.js
+++ test/index.js
@@ -1,6 +1,11 @@
 const { join } = require("path");
+const { readFile, writeFileSync } = require("fs");
+const { promisify } = require("util");
+const promReadFile = promisify(readFile);
 
 const packer = require("../src");
-packer(
-  ...["./test_image.bmp", "./test_script.js"].map(file => join(__dirname, file))
-).then(console.log);
+Promise.all(
+  ["./test_image.bmp", "./test_script.js"].map(file => promReadFile(join(__dirname, file)))
+)
+  .then(bufs => packer(...bufs))
+  .then(console.log);
